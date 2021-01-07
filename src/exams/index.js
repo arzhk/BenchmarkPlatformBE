e7725f5b9ac174f7f5ff7e530bf4a56f5c56f636
arzhk <72197750+arzhk@users.noreply.github.com>
2021-01-07 10:31:16 +0000

    Upload files

---
+++ src/exams/index.js
@@ -0,0 +1,169 @@
+const express = require("express");
+const fs = require("fs");
+const uniqid = require("uniqid");
+const path = require("path");
+const { check, validationResult } = require("express-validator");
+const { join } = require("path");
+
+const router = express.Router();
+
+const readFileHandler = async (filename) => {
+  const targetFile = JSON.parse(fs.readFileSync(join(__dirname, filename)).toString());
+  return targetFile;
+};
+
+const writeFileHandler = (writeToFilename, file) => {
+  try {
+    fs.writeFileSync(path.join(__dirname, writeToFilename), JSON.stringify(file));
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+};
+
+const newUserHandler = async (data) => {
+  const newUser = {
+    _id: uniqid(),
+    candidateFirstName: data.firstName,
+    candidateSurname: data.surname,
+    password: data.password,
+    exams: [{ _examId: 123, examName: "admission test", isCompleted: false, examDuration: 60, questions: [] }],
+  };
+  return newUser;
+};
+
+const errorMessage = (value, message, param = "_id", url = "url") => {
+  const err = new Error();
+  err.message = {
+    errors: [
+      {
+        value: value,
+        msg: message,
+        param: param,
+        location: url,
+      },
+    ],
+  };
+  err.httpStatusCode = 400;
+  return err;
+};
+
+router.get("/questions", async (req, res, next) => {
+  try {
+    if (req.query.id === "123") {
+      const questions = await readFileHandler("questions.json");
+      res.send(questions);
+    } else {
+      res.send("You are not authorised to access this information");
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+router.get("/user/:id", async (req, res, next) => {
+  try {
+    if (req.query.id === "123") {
+      const users = await readFileHandler("users.json");
+      const indexOfUser = users.findIndex((user) => user._id === req.params.id);
+      if (indexOfUser !== -1) {
+        res.send(users[indexOfUser]);
+      } else {
+        res.send(errorMessage(req.params.id, "User with that ID not found"));
+      }
+    } else {
+      let error = new Error();
+      error.httpStatusCode = 401;
+      next(error);
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+router.post(
+  "/register",
+  [
+    check("firstName")
+      .isLength({ min: 3 })
+      .withMessage("First name is too short!")
+      .exists()
+      .withMessage("First Name is missing."),
+    check("surname")
+      .isLength({ min: 3 })
+      .withMessage("Surname is too short!")
+      .exists()
+      .withMessage("Surname is missing."),
+    check("password")
+      .isLength({ min: 5 })
+      .withMessage("Password is too short!")
+      .exists()
+      .withMessage("Password is missing."),
+  ],
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        const err = new Error();
+        err.message = errors;
+        err.httpStatusCode = 400;
+        next(err);
+      } else {
+        let usersFile = await readFileHandler("users.json");
+        const newUser = await newUserHandler(req.body);
+        usersFile.push(newUser);
+        writeFileHandler("users.json", usersFile);
+        res.send(newUser);
+      }
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+);
+
+router.post("/:examId/start", async (req, res, next) => {
+  try {
+    if (req.params.examId === "123") {
+      if (req.query.userID) {
+        let users = await readFileHandler("users.json");
+        const indexOfUser = users.findIndex((user) => user._id === req.query.userID);
+        const indexOfExam = users[indexOfUser].exams.findIndex((exam) => exam._examId == req.params.examId);
+        if (indexOfUser !== -1) {
+          if (users[indexOfUser].exams[indexOfExam].isCompleted !== true) {
+            let questions = await readFileHandler("questions.json");
+            let randomIndexes = [];
+            console.log(questions.length);
+
+            while (randomIndexes.length < 5) {
+              randomIndexes.push(Math.floor(Math.random() * questions.length));
+              randomIndexes = [...new Set(randomIndexes)];
+            }
+
+            for (let i = 0; i < randomIndexes.length; i++) {
+              users[indexOfUser].exams[indexOfExam].questions.push(questions[randomIndexes[i]]);
+            }
+
+            res.send(users[indexOfUser].exams[indexOfExam].questions);
+          } else {
+            res.send(errorMessage(req.params.examId, "User has already completed this exam", "?userID="));
+          }
+          res.send("aaa");
+        } else {
+          res.send(errorMessage(req.query.userID, "User with that ID not found"));
+        }
+      } else {
+        res.send(errorMessage("", "userID is missing", "?userID="));
+      }
+    } else {
+      res.send("Exam with that ID not found");
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+module.exports = router;
---
+++ src/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const examsRoutes = require("./exams");
+const listEndpoints = require("express-list-endpoints");
+const {
+  notFoundHandler,
+  unauthorizedHandler,
+  forbiddenHandler,
+  badRequestHandler,
+  catchAllHandler,
+} = require("./errorHandling");
+
+const server = express();
+
+const port = process.env.PORT || 3001;
+
+const loggerMiddleware = (req, res, next) => {
+  console.log(`Logged ${req.url} ${req.method} -- ${new Date()}`);
+  next();
+};
+
+server.use(cors());
+server.use(express.json());
+server.use(loggerMiddleware);
+
+server.use("/exams", examsRoutes);
+
+server.use(notFoundHandler);
+server.use(unauthorizedHandler);
+server.use(forbiddenHandler);
+server.use(badRequestHandler);
+server.use(catchAllHandler);
+
+console.log(listEndpoints(server));
+
+server.listen(port, () => {
+  console.log("Server is running on port: ", port);
+});
